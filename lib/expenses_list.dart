af51e0274c571991ea9208fd6d01b173f21030ef
ankur-b <ankurbarve29@gmail.com>
2025-08-05 19:20:40 +0530

    deleting a expense

--- lib/expenses_list.dart
+++ lib/expenses_list.dart
@@ -3,16 +3,23 @@ import 'package:expense_tracker/Models/expense.dart';
 import 'package:expense_tracker/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({super.key, required this.onRemoveExpense ,required this.expenses});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (ctx, index) {
-        return ExpenseItem(expense: expenses[index]);
+        return Dismissible(
+          key: ValueKey(expenses[index]),
+          onDismissed: (direction){
+            onRemoveExpense(expenses[index]);
+          },
+          child: ExpenseItem(expense: expenses[index]),
+        );
       },
     );
   }
--- lib/new_expense.dart
+++ lib/new_expense.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/Models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({required this.onAddExpense , super.key});
+  const NewExpense({required this.onAddExpense, super.key});
   final void Function(Expense expense) onAddExpense;
 
   @override
@@ -29,23 +29,43 @@ class _NewExpenseState extends State<NewExpense> {
       _selectedDate = pickedDate;
     });
   }
-  void _submitExpenseData(){
+
+  void _submitExpenseData() {
     final enteredAmount = double.tryParse(_amountController.text);
-    final amountIsInvalid = enteredAmount == null || enteredAmount <=0;
-    if(_titleController.text.trim().isEmpty || amountIsInvalid || _selectedDate == null){
-      showDialog(context: context, builder: (ctx)=>AlertDialog(
-        title: const Text('Invalid input'),
-        content: const Text('Please make sure a valid title, amount, date and category was entered.'),
-        actions: [
-          TextButton(onPressed: (){
-            Navigator.pop(ctx);
-          }, child: const Text('Okay'))
-        ],
-      ));
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text(
+            'Please make sure a valid title, amount, date and category was entered.',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
+            ),
+          ],
+        ),
+      );
       return;
     }
-    widget.onAddExpense(Expense(title: _titleController.text, amount: enteredAmount, date: _selectedDate!, category: _selectedCategory));
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory,
+      ),
+    );
+    Navigator.pop(context);
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -57,7 +77,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16,48,16,16),
       child: Column(
         children: [
           TextField(
@@ -97,7 +117,7 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
-          const SizedBox(height: 16,),
+          const SizedBox(height: 16),
           Row(
             children: [
               DropdownButton(
@@ -111,7 +131,7 @@ class _NewExpenseState extends State<NewExpense> {
                     )
                     .toList(),
                 onChanged: (value) {
-                  if(value==null){
+                  if (value == null) {
                     return;
                   }
                   setState(() {
@@ -128,7 +148,7 @@ class _NewExpenseState extends State<NewExpense> {
               ),
               ElevatedButton(
                 onPressed: () {
-                 _submitExpenseData();
+                  _submitExpenseData();
                 },
                 child: const Text('Save Expense'),
               ),
